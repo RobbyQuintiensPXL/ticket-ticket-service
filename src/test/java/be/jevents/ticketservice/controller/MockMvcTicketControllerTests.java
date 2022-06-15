88033a2d8f187bcd835252ba7da7dbe027d0a216
Robby <robby@Robbys-MacBook-Pro.local>
2022-06-15 20:34:23 +0200

    unit tests

--- src/test/java/be/jevents/ticketservice/controller/MockMvcTicketControllerTests.java
+++ src/test/java/be/jevents/ticketservice/controller/MockMvcTicketControllerTests.java
@@ -1,8 +1,11 @@
 package be.jevents.ticketservice.controller;
 
+import be.jevents.ticketservice.createresource.CreateFullTicketResource;
 import be.jevents.ticketservice.dto.TicketDTO;
 import be.jevents.ticketservice.model.Ticket;
+import be.jevents.ticketservice.model.TicketUser;
 import be.jevents.ticketservice.service.TicketService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -18,9 +21,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -42,11 +46,15 @@ public class MockMvcTicketControllerTests {
     private Ticket ticket;
 
     public void init(){
+        TicketUser ticketUser = new TicketUser();
+        ticketUser.setId(1L);
         ticket = new Ticket();
         ticket.setId(1L);
         ticket.setEventId(1L);
+        ticket.setTicketNumber(1);
         ticket.setStatus("PAYED");
         ticket.setUsername("Username");
+        ticket.setTicketUser(ticketUser);
     }
 
     @Test
@@ -75,6 +83,56 @@ public class MockMvcTicketControllerTests {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").value(2));
+    }
+
+    @Test
+    public void getTicketsByUserTest() throws Exception {
+        init();
+        List<TicketDTO> ticketDTOList = new LinkedList<>();
+        ticketDTOList.add(new TicketDTO(ticket));
+        when(ticketService.getEventsByUser(anyString()))
+                .thenReturn(ticketDTOList);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/tickets/user")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void createTicketTest() throws Exception {
+        init();
+        List<TicketDTO> ticketDTOList = new LinkedList<>();
+        ticketDTOList.add(new TicketDTO(ticket));
+        when(ticketService.getEventsByUser(anyString()))
+                .thenReturn(ticketDTOList);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.post("/tickets/{eventId}/order", 1)
+                        .content(asJsonString(new CreateFullTicketResource(1L, "user",
+                                "Lastname", "Firstname", "street", "city",
+                                3500, "Country", "email", 1)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void validateTicketTest() throws Exception {
+        init();
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/tickets/validate/{eventId}/{ticketNumber}/{ticketUser}",
+                                1, ticket.getTicketNumber(), ticket.getTicketUser().getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
